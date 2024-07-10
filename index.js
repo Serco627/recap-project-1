c88eca3f33bb7e5d268482c480011afd51110ef4
Sercan Sebil <sercansebil95@googlemail.com>
2024-07-10 14:27:34 +0200

    fix some things to continue with recap-2 tasks

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-const button1 = document.querySelector("[data-js='button1']");
+// const button1 = document.querySelector("[data-js='button1']");
 
-button1.addEventListener("click", () => {
-  if (button1.innerText === "Show Answer") {
-    button1.innerText = "The correct answer is: flex-direction: column";
-  } else {
-    button1.innerText = "Show Answer";
-  }
-});
+// button1.addEventListener("click", () => {
+//   if (button1.innerText === "Show Answer") {
+//     button1.innerText = "The correct answer is: flex-direction: column";
+//   } else {
+//     button1.innerText = "Show Answer";
+//   }
+// });
 
-const button2 = document.querySelector("[data-js='button2']");
+// const button2 = document.querySelector("[data-js='button2']");
 
-button2.addEventListener("click", showAnswer);
+// button2.addEventListener("click", showAnswer);
 
-function showAnswer() {
-  button2.innerText === "Show Answer"
-    ? (button2.innerText = "position: static/relative/absolute/fixed/sticky")
-    : (button2.innerText = "Show Answer");
-}
+// function showAnswer() {
+//   button2.innerText === "Show Answer"
+//     ? (button2.innerText = "position: static/relative/absolute/fixed/sticky")
+//     : (button2.innerText = "Show Answer");
+//}
 
 const darkModeButton = document.querySelector("[data-js='darkmode']");
 
