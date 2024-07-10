c236da1d211eaecc16184f83c837ae88403faaaa
Sercan Sebil <sercansebil95@googlemail.com>
2024-07-10 15:22:17 +0200

    add button functionality

--- index.js
+++ index.js
@@ -19,15 +19,29 @@
 //}
 
 const bookmarkButton = document.querySelector("[data-js='bookmark']");
-console.log(bookmarkButton);
 
 bookmarkButton.addEventListener("click", () => {
   bookmarkButton.classList.toggle("bookmark-icon");
 });
 
-//  When the user clicks the bookmark icon the bookmark icon should change it's visual state (e. g. another color or image)
-// When the user clicks the bookmark icon again the bookmark icon should change to its former style
-// The user can click on the bookmark endlessly and the bookmark will toggle between both stylings
+const cardAnswer = document.querySelector("[data-js='card-answer']");
+console.log(cardAnswer);
+const showAnswerButton = document.querySelector("[data-js='button1']");
+
+showAnswerButton.addEventListener("click", () => {
+  cardAnswer.classList.toggle("card-answer");
+  if (showAnswerButton.textContent === "Show Answer") {
+    showAnswerButton.textContent = "Hide Answer";
+  } else {
+    showAnswerButton.textContent = "Show Answer";
+  }
+});
+
+// When the user clicks on the button the previously hidden answer should be displayed
+// When the user clicks this button again the answer is hidden again
+// The user can click on this button endlessly and the answer will either be displayed or hidden after each click
+// The toggle functionality should be applied by using a class which is named "hidden"
+// If the user clicks on an answer button, we want the button to say "hide answer" when the answer is displayed and "show answer" when the answer is not displayed.
 
 const darkModeButton = document.querySelector("[data-js='darkmode']");
 
